775595587ff847e907b10c1d01cd6f1613a85cf5
Keenan Thomson <keenanthomson@gmail.com>
2019-09-30 07:54:01 -0400

    Updates to client, selector still throwing errors on chart

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,31 +2,34 @@ import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import {Chart} from 'react-google-charts';
 import axios from 'axios';
-import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css'
 
 export const App = () => {
-  const [region, setRegion] = useState('US-CA'); // change this using a selector dropdown
+  const [region, setRegion] = useState('US-MA'); // change this using a selector dropdown
   const [priceData, setPriceData] = useState(undefined)
+  const [loading, setLoading] = useState(true)
   
   useEffect(() => {
-    updateRegionData()
+    updateRegionData();
+    // setLoading(false);
   }, []);
 
-  const updateRegionData = () => {
+  const updateRegionData = (newRegion=region) => {
+    setLoading(true);
     axios
-    .get(`http://localhost:3001/${region}/city`)
+    .get(`http://localhost:3001/${newRegion}/city`)
     .then(response => {
       console.log(response.data)
       setPriceData(response.data)
       setRegion(newRegion)
     })
+    .then(setLoading(false))
     .catch(error => console.log(`Error --> `, error))
   }
 
-  const selectRegion = (e) => {
-    updateRegionData(e);
-  }
+  // const selectRegion = (e) => {
+  //   updateRegionData(e);
+  // }
 
   const loadingDivStyling = {
     margin: '300px 500px'
@@ -34,7 +37,7 @@ export const App = () => {
 
   const options = ['US-AL','US-AK','US-AZ','US-AR','US-CA','US-CO','US-CT','US-DE','US-DC','US-FL','US-GA','US-HI','US-ID','US-IL','US-IN','US-IA','US-KS','US-KY','US-LA','US-ME','US-MT','US-NE','US-NV','US-NH','US-NJ','US-NM','US-NY','US-NC','US-ND','US-OH','US-OK','US-OR','US-MD','US-MA','US-MI','US-MN','US-MS','US-MO','US-PA','US-RI','US-SC','US-SD','US-TN','US-TX','US-UT','US-VT','US-VA','US-WA','US-WV','US-WI','US-WY'];
 
-  if (priceData) {
+  if (!loading) {
     return(
        <div> 
         <Chart
@@ -51,8 +54,7 @@ export const App = () => {
           mapsApiKey="AIzaSyB0CkhQh8kVsw3goTeNsbDNlCrNOuo90Wg"
           rootProps={{ 'data-testid': '1' }}
         />
-        {/* <Dropdown options={options} onChange={selectRegion} value={defaultOption} placeholder="Select a State"></Dropdown> */}
-        <select onChange={selectRegion}>
+        <select onChange={updateRegionData}>
           {options.map((elem, index) => {
             return <option value={elem} key={index}>{elem}</option>
           })}
@@ -60,7 +62,7 @@ export const App = () => {
       </div>
     )
   } else {
-    return (<div style={loadingDivStyling}>LOADING...</div>)
+    return <div style={loadingDivStyling}>LOADING...</div>
   }
 }
 
